7bc1c1f93a8a4843019ae7b0357450d02490adc8
BRicsi <barnusrichi@gmail.com>
2026-01-28 09:41:48 +0100

    Add project files.

---
+++ Datas/Read.cs
@@ -0,0 +1,28 @@
+﻿using libraryGUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libraryGUI.Datas
+{
+    internal class Read
+    {
+        public List<Authors> Readauthors()
+        {
+            try
+            {
+                using (var context = new Models.librarydbContext())
+                {
+                    var authors = context.Authors.ToList();
+                    return authors;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
